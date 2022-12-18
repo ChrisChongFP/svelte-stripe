82511bdd2da9dc904ef320890d42a124b83c0fc0
ChrisChongFP <94324573+ChrisChongFP@users.noreply.github.com>
2022-12-18 20:50:57 +1100

    Updated - subscriptions will work now

--- src/routes/examples/payment-element/+page.svelte
+++ src/routes/examples/payment-element/+page.svelte
@@ -2,27 +2,38 @@
   import { goto } from '$app/navigation'
   import { onMount } from 'svelte'
   import { loadStripe } from '@stripe/stripe-js'
-  import { Elements, PaymentElement, LinkAuthenticationElement } from '$lib'
+  import { Elements, Card, LinkAuthenticationElement, CardNumber, CardExpiry, CardCvc, PaymentElement } from '$lib'
   import { page } from '$app/stores'
+  import { subscribe } from 'svelte/internal'
+  import { localStorageStore } from 'fractils'
 
+  
   let stripe = null
   let clientSecret = null
   let error = null
   let elements
   let processing = false
+  let cardElement;
 
   var url1 = new URL($page.url.href)
   var url = String(url1)
   var c = url1.searchParams.get('email')
   var d = url1.searchParams.get('businessId')
+  var e = url1.searchParams.get('customerId')
 
   let data = {
     fpemail : c,
-    businessId: d
+    businessId: d,
+    customerId: e
   };
   let fpemail = data.fpemail;
   let product = 'price_1KNp0qKOURBwtsKiahwlTtus';
   let businessId = data.businessId;
+
+  let custId = localStorageStore('custId');
+  let customerId = custId;
+
+  console.log(custId)
 
 
   onMount(async () => {
@@ -40,41 +51,105 @@
       },
       body: JSON.stringify({
         product: product,
-        businessId: businessId
+        businessId: businessId,
+        fpemail: fpemail,
+        customerId: customerId ? customerId : null
       })
     })
-    const { clientSecret } = await response.json()
+    const { clientSecret, custId_server } = await response.json()
+    custId = custId_server
 
     return clientSecret
   }
 
+  // function subscibeToProduct(paymentMethod) {
+  //   const response2 = fetch('/examples/payment-element/subscribe', {
+  //     method: 'POST',
+  //     headers: {
+  //       'content-type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       product: product,
+  //       businessId: businessId,
+  //       fpemail: fpemail,
+  //       paymentMethod: paymentMethod
+  //     })
+  //   })
+
+  //   return response2
+  // }
   
 
   async function submit() {
     // avoid processing duplicates
     if (processing) return
-
     processing = true
 
     // confirm payment with stripe
-    const result = await stripe
-      .confirmPayment({
-        elements,
-        redirect: 'if_required'
-      })
+    // const result = await stripe
+    //   .confirmPayment({
+    //     elements,
+    //     redirect: 'if_required'
+    //   })
+
+  const result = await stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: cardElement,
+        billing_details: {
+          name
+        }
+      }
+    })
 
     // log results, for debugging
-    console.log({result})
-
+    // console.log({result})
     if (result.error) {
       // payment failed, notify user
       error = result.error
       processing = false
     } else {
+      // console.log("Magic below")
+      // subscibeToProduct(result)
+      // console.log(result)
+
       // payment succeeded, redirect to "thank you" page
       goto('/examples/payment-element/thanks')
     }
   }
+
+  // async function submit() {
+  //   // avoid processing duplicates
+  //   if (processing) return
+
+  //   processing = true
+
+  //   // confirm payment with stripe
+  //   const result = await stripe
+  //     .confirmCardPayment(clientSecret, {
+  //       payment_method: {
+  //         card: cardElement,
+  //         billing_details: {
+  //           email: fpemail
+  //         }
+  //       }
+  //   })
+  //   .then(function(result) {
+  //     subscibeToProduct(result.payment_method.Card)
+  //   });
+
+  //   // log results, for debugging
+  //   console.log({result})
+
+  //   if (result.error) {
+  //     // payment failed, notify user
+  //     error = result.error
+  //     processing = false
+  //   } else {
+  //     // payment succeeded, redirect to "thank you" page
+  //     goto('/examples/payment-element/thanks')
+  //   }
+  // }
 </script>
 
 <h1>Payment Element Example</h1>
@@ -88,7 +163,7 @@
 {/if}
 
 {#if stripe && clientSecret}
-  <Elements
+  <!-- <Elements
     {stripe}
     {clientSecret}
     theme="flat"
@@ -98,7 +173,7 @@
     bind:elements>
     <form on:submit|preventDefault={submit}>
       <LinkAuthenticationElement/>
-      <PaymentElement/>
+      <Card/>
       <input name="businessId" bind:value={businessId} placeholder="BusinessId" type="text" disabled={processing} />
       <input name="product" bind:value={product} type="text" disabled={processing} />
 
@@ -110,7 +185,60 @@
         {/if}
       </button>
     </form>
+  </Elements> -->
+
+  <Elements
+    {stripe}
+    {clientSecret}
+    theme="none"
+    labels="floating"
+    variables={{colorPrimary: '#7c4dff'}}
+    rules={{'.Input': { border: 'solid 1px #0002' }}}
+    bind:elements>
+    <form on:submit|preventDefault={submit}>
+      <input name="businessId" bind:value={businessId} placeholder="BusinessId" type="text" disabled={processing} />
+      <input name="product" bind:value={product} type="text" disabled={processing} />
+      <input name="fpemail" bind:value={fpemail} type="text" disabled={processing} />
+
+      <!-- <PaymentElement/> -->
+      <CardNumber bind:element={cardElement} classes={{base: 'input'}}/>
+
+      <div class="row">
+        <CardExpiry classes={{base: 'input'}}/>
+        <CardCvc classes={{base: 'input'}}/>
+      </div>
+
+      <button disabled={processing}>
+        {#if processing}
+          Processing...
+        {:else}
+          Pay
+        {/if}
+      </button>
+    </form>
   </Elements>
+
+  <!-- <Elements
+    {stripe}
+    {clientSecret}
+    theme="none"
+    labels="floating"
+    variables={{colorPrimary: '#7c4dff'}}
+    rules={{'.Input': { border: 'solid 1px #0002' }}}
+    bind:elements>
+    <form on:submit|preventDefault={submit}>
+      <LinkAuthenticationElement/>
+
+      <input name="businessId" bind:value={businessId} placeholder="BusinessId" type="text" disabled={processing} />
+      <input name="product" bind:value={product} type="text" disabled={processing} />
+      <input name="fpemail" bind:value={fpemail} type="text" disabled={processing} />
+      <Card/>
+  
+      <button>Pay</button>
+    </form>
+  </Elements>
+   -->
+
 {:else}
   Loading...
 {/if}
@@ -126,6 +254,18 @@
     flex-direction: column;
     gap: 10px;
     margin: 2rem 0;
+  }
+
+  
+  input, :global(.input) {
+    border: solid 1px var(--gray-color);
+    padding: 1rem;
+    border-radius: 5px;
+    background: white;
+  }
+
+  .row :global(.input) {
+    width: 20%;
   }
 
   button {
